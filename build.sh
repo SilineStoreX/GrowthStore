11abe7c5d5cfd1decb152b1e6a79bd4283824e6b
poethxp <longzou@hotmail.com>
2024-09-03 15:42:55 +0800

    save

--- build.sh
+++ build.sh
@@ -13,5 +13,6 @@ cp -r front/dist/* target/dist/assets/management/
 cp target/release/chimes-starter target/dist/
 cp target/release/store-server target/dist/
 rm -f target/GrowthStore-Linux.zip
-tar -czvf target/GrowthStore-Linux.tar.gz  target/dist/*
+cd target/dist/
+tar -czvf ../GrowthStore-Linux.tar.gz assets/  chimes-starter  store-server 
 
